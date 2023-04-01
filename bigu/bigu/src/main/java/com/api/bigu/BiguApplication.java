74eaa4b2d18efdabd793f2b7a27dc92781d03bb3
Matheus Alves <matalves12345@gmail.com>
2023-04-01 11:49:20 -0300

    commit inicial

---
+++ bigu/bigu/src/main/java/com/api/bigu/BiguApplication.java
@@ -0,0 +1,13 @@
+package com.api.bigu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiguApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiguApplication.class, args);
+	}
+
+}
