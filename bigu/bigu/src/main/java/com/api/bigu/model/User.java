fc3a67d06adce25f821337c772afff5c9d3c17fd
italoaraujooj <matheus.ramalho1354@gmail.com>
2023-05-03 22:20:55 -0300

    teste: commit inicial

---
+++ bigu/bigu/src/main/java/com/api/bigu/model/User.java
@@ -0,0 +1,13 @@
+package com.api.bigu.model;
+
+import java.util.*;
+
+public class User{
+    protected double matricula;
+
+    protected String name;
+
+    protected String email;
+
+    protected String senha;
+}
