cd06493f4a27bd71e97dcb99041af1f74aaccdd3
Vinicius Xavier <vinicius.muniz@ccc.ufcg.edu.br>
2023-06-23 12:05:29 -0300

    fix: added custom datasource for agnostic database url.

--- src/main/java/com/api/bigu/config/ApplicationConfig.java
+++ src/main/java/com/api/bigu/config/ApplicationConfig.java
@@ -1,10 +1,10 @@
 package com.api.bigu.config;
 
 import com.api.bigu.repositories.UserRepository;
-import com.api.bigu.services.CarService;
 import com.api.bigu.services.EmailService;
 import com.api.bigu.services.JavaMailEmailService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -16,11 +16,31 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
 @Configuration
 @RequiredArgsConstructor
 public class ApplicationConfig {
 
     private final UserRepository userRepository;
+
+    @Bean
+    public DataSource getDataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=prefer";
+
+        DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.postgresql.Driver");
+        dataSourceBuilder.url(dbUrl);
+        dataSourceBuilder.username(username);
+        dataSourceBuilder.password(password);
+        return dataSourceBuilder.build();
+    }
 
     @Bean
     public EmailService emailService() {
